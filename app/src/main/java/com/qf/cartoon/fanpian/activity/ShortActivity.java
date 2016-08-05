ca4617a5dd1673300e10cf36268676ab4e9e9371
saviortyrio <typetheway@163.com>
2016-08-05 10:00:11 +0800

    activity

---
+++ app/src/main/java/com/qf/cartoon/fanpian/activity/ShortActivity.java
@@ -0,0 +1,15 @@
+package com.qf.cartoon.fanpian.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qf.cartoon.fanpian.R;
+
+public class ShortActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_short);
+    }
+}
